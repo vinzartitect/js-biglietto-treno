6d23ec08c03c0455e5abfd0436c16f34e8b2cc29
VincenzoBorriello89 <vincborr89@gmail.com>
2022-03-09 17:30:50 +0100

    esercizio completato

--- asset/js/main.js
+++ asset/js/main.js
@@ -7,21 +7,33 @@ const inputEta = prompt("inserisci la tua età anagrafica");
 console.log(inputEta);
 
 // calcolare i km da percorrere x 0.21 euro al km
-const prezzoBase = inputKm * 0.21;
-// prezzoBase = prezzoBase.toFixed(2);
-console.log('Prezzo Base: ' + prezzoBase + '€');
+let prezzoBase = inputKm * 0.21;
+console.log('Prezzo Base: ' + prezzoBase.toFixed(2) + '€');
 
 //condizione if per calcolare sconto minorenne
+let prezzoFinale;
+
 if (inputEta < 18) {
+
     const scontoMin = prezzoBase * 0.20;
-    const prezzoScontoMin = (prezzoBase - scontoMin);
-    prezzoSconto = prezzoScontoMin.toFixed(2);
-    console.log('Prezzo Scontato: ' + prezzoScontoMin + '€');
+    let prezzoScontoMin = (prezzoBase - scontoMin);
+    console.log('Prezzo Scontato: ' + prezzoScontoMin.toFixed(2) + '€');
+    prezzoFinale = prezzoScontoMin;
+
 }  else if (inputEta >=65) {
+
     const scontoMax = prezzoBase * 0.40;
-    const prezzoScontoMax = Math.floor(prezzoBase - scontoMax);
-    prezzoSconto = prezzoScontoMax.toFixed(2);
-    console.log('Prezzo Scontato: ' + prezzoScontoMax + '€');
+    let prezzoScontoMax = (prezzoBase - scontoMax);
+    console.log('Prezzo Scontato: ' + prezzoScontoMax.toFixed(2) + '€');
+    prezzoFinale = prezzoScontoMax;
+
 }  else {
-    console.log('Prezzo definitivo: ' + prezzoBase + '€');
+
+    console.log('Prezzo definitivo: ' + prezzoBase.toFixed(2) + '€');
+    prezzoFinale = prezzoBase;
+
 }
+
+prezzoFinale = prezzoFinale.toFixed(2);
+
+document.getElementById('totale').innerHTML = `prezzo finale: ${prezzoFinale} €`;
