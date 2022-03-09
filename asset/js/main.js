9eb8fe9e107a2c6ee76affb5a5356b22e3a95728
VincenzoBorriello89 <vincborr89@gmail.com>
2022-03-09 15:51:16 +0100

    creazione if sugli sconti

--- asset/js/main.js
+++ asset/js/main.js
@@ -9,3 +9,22 @@ console.log(inputEta);
 // calcolare i km da percorrere x 0.21 euro al km
 const prezzoBase = inputKm * 0.21;
 console.log('Prezzo Base: ' + prezzoBase + '€');
+
+//condizione if per calcolare sconto minorenne
+if (inputEta < 18) {
+    const scontoMin = prezzoBase * 0.2;
+    const prezzoSconto = Math.floor(prezzoBase - scontoMin);
+    prezzoSconto = prezzoSconto.toFixed(2);
+    console.log('Prezzo Scontato: ' + prezzoSconto + '€');
+}  else {
+    console.log('Prezzo definitivo: ' + prezzoBase + '€');
+}
+
+//condizione if per calcolare sconto maggiore di 65 anni
+if (inputEta >=65) {
+    const scontoMax = prezzoBase * 0.4;
+    const prezzoSconto = Math.floor(prezzoBase - scontoMax);
+    console.log('Prezzo Scontato: ' + prezzoSconto + '€');
+}  else {
+    console.log('Prezzo definitivo: ' + prezzoBase + '€');
+}
