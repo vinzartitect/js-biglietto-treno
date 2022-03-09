c585316a631dc637d7224a7541e0a5b560d8ef81
VincenzoBorriello89 <vincborr89@gmail.com>
2022-03-09 16:04:01 +0100

    unione if sconto min e max

--- asset/js/main.js
+++ asset/js/main.js
@@ -12,19 +12,15 @@ console.log('Prezzo Base: ' + prezzoBase + '€');
 
 //condizione if per calcolare sconto minorenne
 if (inputEta < 18) {
-    const scontoMin = prezzoBase * 0.2;
-    const prezzoSconto = Math.floor(prezzoBase - scontoMin);
-    prezzoSconto = prezzoSconto.toFixed(2);
-    console.log('Prezzo Scontato: ' + prezzoSconto + '€');
-}  else {
-    console.log('Prezzo definitivo: ' + prezzoBase + '€');
-}
-
-//condizione if per calcolare sconto maggiore di 65 anni
-if (inputEta >=65) {
-    const scontoMax = prezzoBase * 0.4;
-    const prezzoSconto = Math.floor(prezzoBase - scontoMax);
-    console.log('Prezzo Scontato: ' + prezzoSconto + '€');
+    const scontoMin = prezzoBase * 0.20;
+    const prezzoScontoMin = Math.floor(prezzoBase - scontoMin);
+    prezzoSconto = prezzoScontoMin.toFixed(2);
+    console.log('Prezzo Scontato: ' + prezzoScontoMin + '€');
+}  else if (inputEta >=65) {
+    const scontoMax = prezzoBase * 0.40;
+    const prezzoScontoMax = Math.floor(prezzoBase - scontoMax);
+    prezzoSconto = prezzoScontoMax.toFixed(2);
+    console.log('Prezzo Scontato: ' + prezzoScontoMax + '€');
 }  else {
     console.log('Prezzo definitivo: ' + prezzoBase + '€');
 }
